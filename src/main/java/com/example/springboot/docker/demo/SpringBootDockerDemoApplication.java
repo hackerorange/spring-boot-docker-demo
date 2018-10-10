c8fbbc93783f004fec7985ae7af2af441e3584fb
zhongct <zhongct@glodon.com>
2018-10-10 14:08:50 +0800

    提交代码

--- src/main/java/com/example/springboot/docker/demo/SpringBootDockerDemoApplication.java
+++ src/main/java/com/example/springboot/docker/demo/SpringBootDockerDemoApplication.java
@@ -2,11 +2,19 @@ package com.example.springboot.docker.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootDockerDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDockerDemoApplication.class, args);
+    }
+
+    @RequestMapping("")
+    public String test() {
+        return "success";
     }
 }
