2c3784f921260805a9507d84cd05ca26859cf057
zhongct <zhongct@glodon.com>
2018-10-10 14:05:34 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot/docker/demo/SpringBootDockerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerDemoApplication.class, args);
+    }
+}
